8226fce66c0a65e4da52e3b3215544545234f29a
pluto <58866685+urMajexty@users.noreply.github.com>
2023-07-12 02:54:52 +0700

    Made a lotta changes
    made the registration for for customers work with Database, and made multiple pages

--- app.py
+++ app.py
@@ -16,15 +16,39 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    resultValue =  cur.execute("SELECT * FROM vehicle")
+    print(resultValue)
+    if resultValue > 0:
+        blogs = cur.fetchall()
+        cur.close()
+        return render_template('index.html', blogs=blogs)
+    cur.close()
+    return render_template('index.html', blogs=None)
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
-@app.route('/blogs/<int:id>/')
-def blogs(id):
-    return render_template('blogs.html', blog_id = id)
+@app.route('/location/')
+def location():
+    return render_template('location.html')
+
+@app.route('/reservation/')
+def reservation():
+    return render_template('reservation.html')
+
+@app.route('/car/')
+def car():
+    return render_template('car.html')
+
+@app.route('/suv/')
+def suv():
+    return render_template('suv.html')
+
+@app.route('/truck/')
+def truck():
+    return render_template('truck.html')
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -34,25 +58,28 @@ def register():
         userDetails = request.form
 
         # Check the password and confirm password
-        if userDetails['password'] != userDetails['confirm_password']:
-            flash('Passwords do not match!', 'danger')
-            return render_template('register.html')
+        #if userDetails['password'] != userDetails['confirm_password']:
+        #    flash('Passwords do not match!', 'danger')
+        #    return render_template('register.html')
 
-        p1 = userDetails['first_name']
-        p2 = userDetails['last_name']
-        p3 = userDetails['username']
-        p4 = userDetails['email']
-        p5 = userDetails['password']
-
-        hashed_pw = generate_password_hash(p5)
-        print(p1 + "," + p2 + "," + p3 + "," + p4 + "," + p5 + "," + hashed_pw)
+        p1 = userDetails['customer_firstname']
+        p2 = userDetails['customer_lastname']
+        p3 = userDetails['customer_dob']
+        p4 = userDetails['customer_age']
+        p5 = userDetails['customer_gender']
+        p6 = userDetails['customer_email']
+        p7 = userDetails['customer_phone_number']
+        p8 = userDetails['customer_address']
+        p9 = userDetails['customer_identification_number']
+        p10 = userDetails['customer_passport']
+        
+        print(p1 + "," + p2 + "," + p3 + "," + p4 + "," + p5 + "," + p6 + "," + p7 + "," + p8 + "," + p9 + "," + p10)
 
         queryStatement = (
             f"INSERT INTO "
-            f"user(first_name,last_name, username, email, password, role_id) "
-            f"VALUES('{p1}', '{p2}', '{p3}', '{p4}','{hashed_pw}', 1)"
+            f"customer(customer_firstname,customer_lastname, customer_dob, customer_age, customer_gender, customer_email, customer_phone_number, customer_address, customer_identification_number, customer_passport) "
+            f"VALUES('{p1}', '{p2}', '{p3}', '{p4}','{p5}','{p6}','{p7}','{p8}','{p9}','{p10}')"
         )
-        print(check_password_hash(hashed_pw, p5))
         print(queryStatement)
         cur = mysql.connection.cursor()
         cur.execute(queryStatement)
@@ -99,28 +126,47 @@ def login():
         return redirect('/')
     return render_template('login.html')
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    flash("You have been logged out", 'info')
-    return redirect('/')
-
 @app.route('/write-blog/', methods=['GET', 'POST'])
 def write_blog():
+    try:
+        username = session['username']
+    except:
+        flash('Please sign in first', 'danger')
+        return redirect('/login')
+    if request.method == 'POST':
+        blogpost = request.form
+        title = blogpost['title']
+        body = blogpost['body'] 
+        cur = mysql.connection.cursor()
+        queryStatement = (
+            f"INSERT INTO blog(title, body, username) "
+            f"VALUES('{title}','{body}','{username}')"
+        )
+        print(queryStatement)
+        cur.execute(queryStatement)
+        mysql.connection.commit()
+        cur.close()
+        flash("Successfully posted", 'success')
+        return redirect('/')
     return render_template('write-blog.html')
 
 @app.route('/my-blogs/')
 def my_blogs():
-    return render_template('my-blogs.html')
+    try:
+        username = session['username']
+    except:
+        flash('Please sign in first', 'danger')
+        return redirect('/login')
 
-@app.route('/edit-blog/<int:id>/', methods=['GET', 'POST'])
-def edit_blog(id):
-    return render_template('edit-blog.html')
-
-@app.route('/delete-blog/<int:id>/', methods=['POST'])
-def delete_blog(id):
-    return 'success'
-
+    cur = mysql.connection.cursor()
+    queryStatement = f"SELECT * FROM blog WHERE username = '{username}'"
+    print(queryStatement)
+    result_value = cur.execute(queryStatement) 
+    if result_value > 0:
+        my_blogs = cur.fetchall()
+        return render_template('my-blogs.html', my_blogs=my_blogs)
+    else:
+        return render_template('my-blogs.html',my_blogs=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
